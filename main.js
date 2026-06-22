a29eae8e445e7eb2529602caea233cfa10243e81
jx <jxaop1@gmail.com>
2026-06-22 15:17:16 +0000

    aula 5 part 1

--- main.js
+++ main.js
@@ -3,19 +3,13 @@ let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';''
+
+campoSenha.value = letrasMaiusculas;
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
-
-function diminuiTamanho(){
-    tamanhoSenha = tamanhoSenha-1;
-    numeroSenha.textContent = tamanhoSenha;
-}
-
-function aumentaTamanho(){
-    tamanhoSenha = tamanhoSenha+1;
-    numeroSenha.textContent = tamanhoSenha;
-}
 
 function diminuiTamanho(){
     if (tamanhoSenha > 1){
@@ -23,13 +17,27 @@ function diminuiTamanho(){
         tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
-
 function aumentaTamanho(){
     if (tamanhoSenha < 20){
        // tamanhoSenha = tamanhoSenha+1;
        tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
+
+
+
+function geraSenha(){
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha;i++){
+        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[numeroAleatorio];
+    }
+    campoSenha.value = senha;
+}
+
 
